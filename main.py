d572b1455525777137ff0a9be255416e730b9893
Kyuu <69904775+KyuuLee@users.noreply.github.com>
2022-09-28 23:41:14 -0700

    1

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 # ******************************
 # Make your Code
 # ******************************
+
+# numbers = list(map(int, input().split()))
+numbers = [5, 25, 15, 10, 0]
+print (numbers)
+
+for i in range(len(numbers)):
+	minidx = numbers.index(min(numbers[i:]))
+	numbers[i], numbers[minidx] = numbers[minidx], numbers[i]
+	print (numbers)
